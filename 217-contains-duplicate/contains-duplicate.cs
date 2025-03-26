f50065e24734b7a50c4892153a14ce98a3656dad
rupakds <41875149+rupakds@users.noreply.github.com>
2025-03-26 21:06:00 +0530

    Time: 5 ms (98.41%) | Memory: 68.4 MB (39.34%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public bool ContainsDuplicate(int[] nums) {
+        
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+
+        for(int i = 0; i < nums.Length; i++){
+            if(dict.ContainsKey(nums[i])){
+                return true;
+            }else{
+                dict[nums[i]] = i;
+            }
+        }
+        return false;
+    }
+}
