7564530e4ba3202866a651eb6544f328c37201a1
rupakds <41875149+rupakds@users.noreply.github.com>
2025-04-08 21:46:15 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (13.25%) - LeetSync

--- 75-sort-colors/sort-colors.cs
+++ 75-sort-colors/sort-colors.cs
@@ -1,17 +1,18 @@
 public class Solution {
     public void SortColors(int[] nums) {
-        int i = 0, j =0;
         int n = nums.Length;
-
-        for(i =0; i < n-1; i++){
-            for(j =0; j < n-i-1; j++){
-                if(nums[j] > nums[j + 1]){
-                   int temp = nums[j];
-                   nums[j] = nums[j + 1];
-                   nums[j + 1] = temp;
-                }
-                  
-            }
+        for(int i = 1; i < n; i++){
+        int key = nums[i];
+        int j = i - 1;
+        
+        while(j >= 0 && nums[j] > key){
+           nums[j + 1] = nums[j];
+        j = j -1;
         }
+
+        nums[j + 1] = key;
+
+        }
+
     }
 }
