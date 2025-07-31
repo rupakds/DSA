aac649272afd2923dcc663c568b92dd7612cac39
rupakds <41875149+rupakds@users.noreply.github.com>
2025-07-31 20:41:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Height of Binary Tree/height-of-binary-tree.cs
+++ Difficulty: Easy/Height of Binary Tree/height-of-binary-tree.cs
@@ -12,36 +12,50 @@ class Node
 }
 */
 class Solution {
-    public int height(Node node) {
-        // code here
+        // public int height(Node node) {
+    //     // code here
 
-        if(node ==  null){
-            return 0;
-        }
+    //     if(node ==  null){
+    //         return -1;
+    //     }
                 
-        Queue<Node> queue = new Queue<Node>();
+    //     Queue<Node> queue = new Queue<Node>();
         
-        queue.Enqueue(node);
-        int depth = -1;
+    //     queue.Enqueue(node);
+    //     int depth = -1;
         
-        while(queue.Count > 0){
+    //     while(queue.Count > 0){
             
-            int levelCount = queue.Count;
+    //         int levelCount = queue.Count;
       
             
-              for(int i = 0 ; i < levelCount; i++){
+    //           for(int i = 0 ; i < levelCount; i++){
                   
-                    Node current = queue.Dequeue();
+    //                 Node current = queue.Dequeue();
                     
-                    if(current.left != null){
-                        queue.Enqueue(current.left);
-                    }
-                     if(current.right != null){
-                        queue.Enqueue(current.right);
-                    }
-              }
-              depth++;
+    //                 if(current.left != null){
+    //                     queue.Enqueue(current.left);
+    //                 }
+    //                  if(current.right != null){
+    //                     queue.Enqueue(current.right);
+    //                 }
+    //           }
+    //           depth++;
+    //     }
+    //     return depth;
+    // }
+    
+    public int height(Node node) {
+        if (node == null) {
+            return -1;
         }
+    
+        int left = height(node.left);
+        int right = height(node.right);
+    
+        int depth = 1 + ((left > right) ? left : right);
+    
         return depth;
     }
+
 }
