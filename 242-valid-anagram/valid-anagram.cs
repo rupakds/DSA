bb20f67763cba1dca7a52372352f4fd405251963
rupakds <41875149+rupakds@users.noreply.github.com>
2025-08-26 21:47:46 +0530

    Time: 6 ms (48.95%) | Memory: 43.8 MB (57.35%) - LeetSync

--- 242-valid-anagram/valid-anagram.cs
+++ 242-valid-anagram/valid-anagram.cs
@@ -1,26 +1,36 @@
 public class Solution {
     public bool IsAnagram(string s, string t) {
-        Dictionary<char, int> dict = new Dictionary<char, int>();
+       
+       Dictionary<char, int> dict = new Dictionary<char, int>();
 
-        foreach(char ch in s){
-            if(dict.ContainsKey(ch)){
-                dict[ch]++;
+        foreach(char c in s){
+
+            if(dict.ContainsKey(c)){
+
+                dict[c]++;  //increment if exist 
+
             }else{
-                dict[ch] = 1;
+                dict[c] = 1; // add new character 
             }
         }
 
         foreach(char ch in t){
-            if(!dict.ContainsKey(ch)){
-                return false; //having extra character 
-            }else{
-                dict[ch]--;
-            }
+
+            if(dict.ContainsKey(ch)){
+
+            dict[ch]--;  //decreament if exist 
 
             if(dict[ch] == 0){
-                dict.Remove(ch); // Remove if count is zero
+
+                dict.Remove(ch); // remove if char value is 0
             }
+        }else{
+            return false;
         }
-   return dict.Count == 0;
+        
+        }
+
+          return dict.Count == 0;
+
     }
 }
